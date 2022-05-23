2adaa1e6490e432cfb79c075b28fad8a2fce0a31
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-23 10:24:57 +0530

    Update soln.cpp

--- ICPC Past Problems/HOLI SPOJ/soln.cpp
+++ ICPC Past Problems/HOLI SPOJ/soln.cpp
@@ -1,78 +1,67 @@
-#include <vector>
-#include <list>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <deque>
-#include <stack>
-#include <bitset>
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <sstream>
 #include <iostream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <cstring>
-#include <queue>
-#include <cassert>
-using namespace std;
-//https://www.spoj.com/problems/HOLI/
-int dfs(unordered_map<int,vector<int>> &m, unordered_map<string,int> &w, int &res, int node, int parent, int n){
-    if(m[node].size()==1 && m[node][0]==parent) return 1;
-    int ans=0;
-    for(int i:m[node]){
-        if(i!=parent){
-            int ret = dfs(m,w,res,i,node,n);
-            string key = to_string(node)+"-"+to_string(i);
-            ans += ret;
-            //cout<<key<<" "<<w[key]<<endl;
-            //cout<<ret<<endl;
-            res += min(ret,n-ret)*2*w[key];
-        }
-    }
-    //cout<<ans<<endl;
-    return 1+ans;
-}
+#include <vector>
 
-int solve(){
-    int n;
-    cin>>n;
-    unordered_map<int,vector<int>> m;
-    unordered_map<string,int> w;
-    int res;
-    res=0;
-    for(int i=0;i<n-1;i++){
-        int x,y,z;
-        cin>>x>>y>>z;
-        m[x].push_back(y);
-        m[y].push_back(x);
-        string key = to_string(x)+"-"+to_string(y);
-        w[key]=z;
-        key = to_string(y)+"-"+to_string(x);
-        w[key]=z;
+using namespace std;
+
+class Graph{
+    int V;
+    vector<vector<pair<int,int>>> l;
+    
+    public:
+    Graph(int v){
+        V = v;
+        l.resize(V);
     }
-    for(int i:m[1]){
-        int ret = dfs(m,w,res,i,1,n);
-        string key = to_string(1)+"-"+to_string(i);
-        // cout<<key<<" "<<w[key]<<endl;
-        // cout<<ret;
-        res += min(ret,n-ret)*2*w[key];
+    
+    void addEdge(int u, int v, int cost){
+        l[u].push_back(make_pair(v,cost));
+        l[v].push_back(make_pair(u,cost));
     }
-    //cout<<res<<endl;
-    return res;
-}
+    
+    int dfs(int node, vector<bool> &vis, vector<long> &count, long &ans){
+        vis[node]=true;
+        count[node]=1;
+        
+        for(auto nbr_pair:l[node]){
+            int nbr = nbr_pair.first;
+            int wt = nbr_pair.second;
+            if(!vis[nbr]){
+                count[node] += dfs(nbr,vis,count,ans);
+                int nx = count[nbr];
+                int ny = V-nx;
+                ans += 2*min(nx,ny)*wt;
+            }
+        }
+        return count[node];
+    }
+    
+    int dfs(){
+        vector<bool> vis(V,false);
+        vector<long> count(V,0);
+        
+        long ans=0;
+        dfs(0,vis,count,ans);
+        return ans;
+    }
+};
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
     cin>>t;
-    for(int i=1;i<=t;i++){
-        int res = solve();
-        cout<<"Case #"<<i<<": "<<res<<endl;
+    int tt=t;
+    while(t--){
+        int v;
+        cin>>v;
+        Graph g(v);
+        for(int i=0;i<v-1;i++){
+            int x,y,z;
+            cin>>x>>y>>z;
+            g.addEdge(x-1,y-1,z);
+        }
+        cout<<"Case #"<<tt-t<<": "<<g.dfs()<<endl;
     }
+    
     return 0;
 }
