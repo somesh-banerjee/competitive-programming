efadb56ddcf060b06139fe1f5b7460664bc59daa
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-28 18:45:33 +0530

    to do q4

---
+++ Kick Start 2022/Round C/Palindromic Deletions (14pts, 27pts)/soln.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int testcase) {
+	cout << "Case #" << testcase << ": ";
+
+}
+/*
+Not done 
+First learn https://youtu.be/l5s2DGHdOyA?t=1517
+*/
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int tt;
+	cin >> tt;
+	for (int i = 1; i <= tt; i++) {
+		solve(i);
+	}
+}
